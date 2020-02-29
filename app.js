9fe0246ee4961565243407c76a88a7971f1b1eb8
RayPhe <rayphelan86@gmail.com>
2020-02-29 12:05:46 +0000

    adding images and js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const game = ()=>{
+const game = () => {
     let pScore = 0;
     let cScore = 0;
 
-    //start the game
-
-    const startGame = () =>{
+    const startGame = () => {
         const playBtn = document.querySelector(".intro button");
         const introScreen = document.querySelector(".intro");
         const match = document.querySelector(".match");
@@ -13,17 +11,81 @@ const game = ()=>{
             introScreen.classList.add("fadeOut");
             match.classList.add("fadeIn");
         });
-   };
-   //Play match
+    };
 
-   const playMatch = () => {
-       const options = document.querySelector(".options button")
-   };
+    // play match
 
-   //calls all inner functions
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
 
-   startGame();
+        const computerOptions = ["rock", "paper", "scissors"];
+
+        // computer choice
+
+        options.forEach(option => {
+            option.addEventListener("click", function () {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+            
+            // Update images
+            playerHand.src = `./assets/${this.textContent}.png`;
+            computerHand.src = `./assets/${computerChoice}.png`;
+            });
+        });
+
+    };
+        // comparing hands
+
+    const compareHands = (playerChoice, computerChoice) =>{
+
+        // checking for a draw
+
+        const winner = document.querySelector(".winner");
+        if(playerChoice === computerChoice){
+            wimmer.textContent = "It's a draw";
+            return;
+        }
+        // checking for rock
+
+        if(playerChoice === "rock"){
+            if(computerChoice === "scissors"){
+                winner.textContent = "Human Wins!"
+                return;
+            }else{
+                winner.textContent = "Machine Wins!"
+                return;
+            }
+        }
+
+        // checking for paper
+
+        if(playerChoice === "paper"){
+            if(computerChoice === "scissors"){
+                winner.textContent = "Machine Wins!"
+                return;
+            }else{
+                winner.textContent = "Human Wins!"
+                return;
+            }
+        }
+
+        //check for scissors
+
+        if(playerChoice === "scissors"){
+            if(computerChoice === "rock"){
+                winner.textContent = "Machine Wins!"
+                return;
+            }else{
+                winner.textContent = "Human Wins!"
+                return;
+            }
+        }
+    }
+
+
+    startGame();
+    playMatch();
 };
-   //start the game function
-
-   game();
+game();
